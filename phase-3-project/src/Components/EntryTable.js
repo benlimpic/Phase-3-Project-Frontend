1a644e4415e1dbb1fcc21b3e337d19db48d5ee8f
Alexandra Davis <alex.davis806@gmail.com>
2022-05-12 10:41:30 -0700

    fixed broken everything

--- phase-3-project/src/Components/EntryTable.js
+++ phase-3-project/src/Components/EntryTable.js
@@ -1,50 +1,48 @@
-
-import Entry from './Entry'
+import { useEffect } from 'react'
 import Table from 'react-bootstrap/Table'
 
 
-const EntryTable = ( { entries } ) => {
-   
-console.log(entries)
-    // const entry = entries.map((entry) => {
-    //     // console.log(entry)
-    //     return ( <Entry  
-    //         entry={entry}
-    //         songName={entry.song.song_name}
-    //         artistName={entry.song.artist_name}
-    //         // {entry.song.genre}
-    //         // {entry.id} 
-    //     />
-    //     )
-    // })
+const EntryTable = ( { entries, deleteEntry } ) => {
+    // useEffect(() => {
+    //     console.log(entries)
+    // }, [entries] )
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:9292/entries/${id}`, {
+          method: "DELETE",
+        });
+        deleteEntry(id)
+    }
 
     return (
 
         <table>
-        <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th>Genre</th>
-          <th>Taste</th>
-          <th>Smell</th>
-          <th>Color</th>
-          <th>Animal</th>
-        </tr>
-        {entries.map((entry) => {
-          return (
-            <tr key={entry.id}>
-              <td>{entry.song.song_name}</td>
-              <td>{entry.song.artist_name}</td>
-              <td>{entry.song.genre}</td>
-              <td>{entry.taste.emoji}</td>
-              <td>{entry.smell.emoji}</td>
-              <td>{entry.color.emoji}</td>
-              <td>{entry.animal.emoji}</td>
-              <button>edit me!</button>
-              <button>delete me!</button>
-            </tr>
-            )
-            })}
+            <tbody>
+                <tr>
+                <th>Song</th>
+                <th>Artist</th>
+                <th>Genre</th>
+                <th>Taste</th>
+                <th>Smell</th>
+                <th>Color</th>
+                <th>Animal</th>
+                </tr>
+                {entries.map((entry) => {
+                return (
+                    <tr key={entry.id}>
+                    <td>{entry.song.song_name}</td>
+                    <td>{entry.song.artist_name}</td>
+                    <td>{entry.song.genre}</td>
+                    <td>{entry.taste.emoji}</td>
+                    <td>{entry.smell.emoji}</td>
+                    <td>{entry.color.emoji}</td>
+                    <td>{entry.animal.emoji}</td>
+                    <td><button>edit me!</button></td>
+                    <td><button onClick={(e) => handleDelete(entry.id)}>delete me!</button></td>
+                    </tr>
+                    )
+                    })}
+            </tbody>
         </table>
     )
 }
